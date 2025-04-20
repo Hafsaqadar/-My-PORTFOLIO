fcff458c6755c8d44d4f916cf0403945c62118d5
hafsaqadar <haf252537@gmail.com>
2025-04-20 09:27:22 +0300

    added scroll animation

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,3 +1,5 @@
+import { RevealOnScroll } from "../RevealOnScroll"
+
 export const About = () => {
 
     const frontendSkills =[
@@ -12,6 +14,7 @@ export const About = () => {
     ]
     return(
         <section id="about" className="min-h-screen flex items-center justify-center py-20">
+        <RevealOnScroll>
        <div className="mx-w-3xl mx-auto px-4">
         <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">About Me   
         </h2>
@@ -51,6 +54,7 @@ export const About = () => {
         </div>
         </div>
        </div>
+       </RevealOnScroll>
        </section>
     )
 }
