070f8baa18218c5034c5653e927ecadd4a0e4f77
hafsaqadar <haf252537@gmail.com>
2025-04-21 08:13:09 +0300

    ADDED TO PROJECT

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -32,10 +32,11 @@ export const Projects = () => {
                 </div>
                </div>
                <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_0_8px_rgba(59,130, 246, 0.2)] transition">
-                <h2 className="text-xl font-bold mb-2 ">Task Manager </h2>
-                <p className="text-gray-400 mb-4">A task manager web application with signup and profile</p>
+                <h2 className="text-xl font-bold mb-2 ">Artist Finder </h2>
+                <p className="text-gray-400 mb-4">The Artist Finder Landing Page is a sleek and modern single-page website designed to promote a music-related platform or app. 
+                    With a strong visual layout and clean sections, it also has Clean and responsive layout, optimized for all devices Stylish typography and layout to create a modern feel</p>
                 <div className="flex flex-wrap gap-2 mb-4">
-                    {["react", 'tailwind','firebase'].map((tech,key)=>(
+                    {["HTML", 'CSS'].map((tech,key)=>(
                       <span 
                       key={key}
                       className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm  hover:bg-blue-500/20 hover:shadow-[0_0_8px_rgba(59,130, 246, 0.1)] transition-all" >
@@ -45,10 +46,10 @@ export const Projects = () => {
                     ))}
                 </div>
                 <div className="flex justify-between items-center  mx-6 mt-4">
-                    <a href=""
+                    <a href="https://artist-finder-website.vercel.app/"
                     className="text-blue-400 hover:text-blue-300 transition-colors my-4">
                         View project  → </a>
-                        <a href=""
+                        <a href="https://github.com/Hafsaqadar/Artist-Finder-website"
                     className="text-blue-400 hover:text-blue-300 transition-colors my-4">View code  → </a>
                 </div>
                </div>
